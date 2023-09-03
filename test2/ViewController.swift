2fb243650ce98db42da30e696052763838d898af
TuringCodesAdnan <142691979+TuringCodesAdnan@users.noreply.github.com>
2023-09-03 14:28:03 +0300

    Initial Commit

---
+++ test2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Mustafa Adnan Tatlıcı on 3.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
